5084db6c2272d7b89974a0dc1f892748eab948eb
jinanzeedat <104310624+jinanzeedat@users.noreply.github.com>
2023-05-02 15:13:08 +0300

    update

--- controllers/course.py
+++ controllers/course.py
@@ -39,3 +39,44 @@ def add_course():
     db.studentsreg.insert(studentID=student_id, courseID=course_id)
     redirect(URL('courses'))
     response.flash = 'Course added to schedule!'
+
+def courses():
+    query = ((db.courses.prerequisties == None) & (~db.courses.code.belongs(db(db.studentsreg.studentID == auth.user.id).select(db.studentsreg.courseID)))) | \
+            ((db.courses.prerequisties != None) & (db.courses.prerequisties.belongs(db(db.studentsreg.status == 'Pass').select(db.studentsreg.courseID)))) & \
+            (~db.courses.code.belongs(db(db.studentsreg.studentID == auth.user.id).select(db.studentsreg.courseID, join=db.courses.on(db.courses.prerequisties == db.studentsreg.courseID)))) | \
+            ((db.courses.prerequisties == None) & (~db.courses.code.belongs(db(db.studentsreg.studentID == auth.user.id).select(db.studentsreg.courseID)))) | \
+            ((db.courses.prerequisties.belongs(db(db.studentsreg.status == 'Pass').select(db.studentsreg.courseID))) & (~db.courses.code.belongs(db(db.studentsreg.studentID == auth.user.id).select(db.studentsreg.courseID)))) | \
+            ((db.courses.prerequisties.belongs(db(db.studentsreg.status == 'Failed').select(db.studentsreg.courseID))) & (db.courses.code.belongs(db(db.studentsreg.studentID == auth.user.id).select(db.studentsreg.courseID))))
+
+    fields = [
+        db.courses.code,
+        db.courses.name,
+        db.courses.capacity,
+        db.courses.instructor,
+        db.courses.description,
+        db.courseschedules.days,
+        db.courseschedules.startTime,
+        db.courseschedules.endTime,
+        db.courseschedules.RoomNo,
+    ]
+    
+    left = [
+        db.courseschedules.on(db.courses.scheduled == db.courseschedules.id),
+    ]
+   
+    links = [lambda row: A('Add', _href=URL('add_course', vars=dict(code=row.courses.code)))]
+    grid = SQLFORM.grid(query, fields=fields, left=left, links=links, create=False, editable=False, deletable=False, csv=False, details=False)
+    return dict(grid=grid)
+
+
+def add_course():
+    course_id = request.vars.code
+    student_id = auth.user.id
+    student_reg = db(db.studentsreg).select(orderby=~db.studentsreg.id).first()
+    if student_reg is None:
+        id = 1
+    else:
+        id = student_reg.id + 1
+    db.studentsreg.insert(id=id, studentID=student_id, courseID=course_id)
+    redirect(URL('courses'))
+    response.flash = 'Course added to schedule!'
