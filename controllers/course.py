41e7f8c9b4e1ff1a217266d03595f86b2baafd70
191081@ppu.edu.ps <201153@ppu.edu.ps>
2023-05-02 11:25:55 +0300

    courses

--- controllers/course.py
+++ controllers/course.py
@@ -26,3 +26,16 @@ def mycourses():
     fields=[db.courses.id,db.courses.name, db.courses.capacity,db.courses.instructor,db.courses.description]
     grid = SQLFORM.grid(query,fields=fields,csv=False,editable=False, create=False,searchable=fields)
     return dict(grid=grid)
+
+
+def courses():
+    links = [lambda row: A('Add', _href=URL('add_course', vars=dict(course_id=row.id)))]
+    grid = SQLFORM.grid(db.courses, links=links, create=False, editable=False, deletable=False,csv=False,details=False)
+    return dict(grid=grid)
+
+def add_course():
+    course_id = request.vars.id
+    student_id = auth.user.id
+    db.studentsreg.insert(studentID=student_id, courseID=course_id)
+    redirect(URL('courses'))
+    response.flash = 'Course added to schedule!'
